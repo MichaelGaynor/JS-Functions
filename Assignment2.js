93b250364eeaeb4a3f0c74f2391641320f2b8719
Michael Gaynor <jmichaelgaynor@gmail.com>
2015-09-30 12:55:25 -0400

    Reformatted assignment 2 to fit better

--- Assignment2.js
+++ Assignment2.js
@@ -1,5 +1,7 @@
 // ---------------------
-// Define a function max() that takes two numbers as arguments and returns the largest of them. Use the if-then-else construct available in JavaScript.
+// Define a function max() that takes two numbers as arguments 
+//and returns the largest of them. Use the if-then-else construct 
+//available in JavaScript.
 // ---------------------
 
 function max(){
@@ -7,7 +9,8 @@ function max(){
 }
 
 // ---------------------
-// Define a function maxOfThree() that takes three numbers as arguments and returns the largest of them.
+// Define a function maxOfThree() that takes three numbers as arguments 
+//and returns the largest of them.
 // ---------------------
 
 function maxOfThree(){
@@ -15,7 +18,8 @@ function maxOfThree(){
 }
 
 // ---------------------
-// Write a function that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise.
+// Write a function that takes a character (i.e. a string of length 1) 
+//and returns true if it is a vowel, false otherwise.
 // ---------------------
 
 function isVowel(char){
@@ -23,7 +27,10 @@ function isVowel(char){
 }
 
 // ---------------------
-// Write a function translate() that will translate a text into "rÃ¶varsprÃ¥ket". That is, double every consonant and place an occurrence of "o" in between. For example, translate("this is fun") should return the string "tothohisos isos fofunon".
+// Write a function translate() that will translate a text into "rÃ¶varsprÃ¥ket". 
+//That is, double every consonant and place an occurrence of "o" in between. 
+//For example, translate("this is fun") should return 
+//the string "tothohisos isos fofunon".
 // ---------------------
 
 function rovarspraket(phrase){
@@ -32,7 +39,8 @@ function rovarspraket(phrase){
 
 
 // ---------------------
-// Define a function reverse() that computes the reversal of a string. For example, reverse("jag testar") should return the string "ratset gaj".
+// Define a function reverse() that computes the reversal of a string. 
+//For example, reverse("jag testar") should return the string "ratset gaj".
 // ---------------------
 
 function reverse(){
@@ -40,7 +48,8 @@ function reverse(){
 }
 
 // ---------------------
-// Write a function findLongestWord() that takes an array of words and returns the length of the longest one.
+// Write a function findLongestWord() that takes an array of words and 
+//returns the length of the longest one.
 // ---------------------
 
 function findLongestWord(words){
@@ -48,7 +57,8 @@ function findLongestWord(words){
 }
 
 // ---------------------
-// Write a function filterLongWords() that takes an array of words and an integer i and returns the array of words that are longer than i.
+// Write a function filterLongWords() that takes an array of words 
+//and an integer i and returns the array of words that are longer than i.
 // ---------------------
 
 function filterLongWords(words, i){
@@ -56,7 +66,10 @@ function filterLongWords(words, i){
 }
 
 // ---------------------
-// Write a function charFreq() that takes a string and builds a frequency listing of the characters contained in it. Represent the frequency listing as a Javascript object. Try it with something like charFreq("abbabcbdbabdbdbabababcbcbab").
+// Write a function charFreq() that takes a string and builds a frequency listing 
+//of the characters contained in it. Represent the frequency listing as a 
+//Javascript object. Try it with something like 
+//charFreq("abbabcbdbabdbdbabababcbcbab").
 // ---------------------
 
 function charFreq(string){
