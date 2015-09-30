9b9543720229642a26bc19ac964207efdd705d73
Michael Gaynor <jmichaelgaynor@gmail.com>
2015-09-30 11:57:44 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,18 @@
+var function foo(data){
+  console.log(data);
+}
+
+var boo = foo;
+
+boo("secret sauce");
+
+
+var obj = {};
+
+var person = {}
+
+var myList = [1, 2, 3, 44, 54];
+myList.pop();
+myList.forEach(function(item){
+  console.log(item);
+})
